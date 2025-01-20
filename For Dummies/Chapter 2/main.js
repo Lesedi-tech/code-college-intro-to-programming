91fca20dbd289c9c69589420d50e46272b70ee65
Lesedi-tech <158996153+Lesedi-tech@users.noreply.github.com>
2025-01-20 22:20:24 +0200

    Add files via upload

---
+++ For Dummies/Chapter 2/main.js
@@ -0,0 +1,14 @@
+// alert("Greetings!!!");
+
+for (var i = 0; i < 300; i++) {
+    document.write("David said, 'Hi class',then carried on with his day");
+};
+
+console.log(" David said, 'Hi class',then carried on with his day ");
+
+var string1 = "string";
+var string2 = 'string';
+
+// single comment
+/* multiline 
+*/
---
+++ For Dummies/Chapter 3/main.js
@@ -0,0 +1,10 @@
+var name;
+
+name = " Lesedi";
+
+console.log(name.length);
+console.log(name.indexOf("s"));
+
+var question = prompt("Is this your name? " + name);
+
+document.write("Welcome to the document " + name);
