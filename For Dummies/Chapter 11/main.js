a420bcc0a509f5afceb2af2e6bbe4bf39f8821db
Lesedi-tech <158996153+Lesedi-tech@users.noreply.github.com>
2025-01-23 08:03:35 +0200

    Add files via upload

---
+++ For Dummies/Chapter 11/main.js
@@ -0,0 +1,14 @@
+/*var parents = ["Parent1 "," Parent2 "];
+var friend = ["Friend1 ", " Friend2 "];
+var numbers = [3, 6, 9];
+
+var family = parents.concat(friend).join("");
+var familia = document.getElementById("myFamily");
+//console.log(family);
+
+
+familia.innerHTML = "<h1>This my family:" + family + "</h1>";*/
+
+
+longarray = [1,2,3,4,5,6,7,8,9,10];
+console.log(longarray.slice(7,9))
